454db2593c28aa126cd14a2d75198018107b58c7
alexz330 <alexislc330@gmail.com>
2022-01-30 19:45:30 -0600

    segunda practica

--- Practica_2/src/ejercicio/pkg1_fecha_/pkg29_01/Ejercicio1_Fecha_29_01.java
+++ Practica_2/src/ejercicio/pkg1_fecha_/pkg29_01/Ejercicio1_Fecha_29_01.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio.pkg2;
+package ejercicio.pkg1_fecha_.pkg29_01;
+
 import java.io.FileInputStream;
 import java.io.FileInputStream;
 
@@ -11,24 +12,28 @@ import java.io.FileInputStream;
  *
  * @author alexi
  */
-public class Ejercicio2 {
+public class Ejercicio1_Fecha_29_01 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         interpretar("entrada.txt");
+        
     }
-    
-     private static void interpretar(String path) {
+
+    private static void interpretar(String path) {
         analizadores.Sintactico pars;
         try {
-            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars = new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
             pars.parse();
-            
+
         } catch (Exception ex) {
             System.out.println("Error fatal en compilación de entrada.");
-            System.out.println("Causa: "+ex.getCause());
-        } 
+            System.out.println("Causa: " + ex.getCause());
+        }
     }
+    
+    
 }
